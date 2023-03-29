cda41c10d51da3b61facc0255433ff9f7af93b03
Iva <vanya12345654@gmail.com>
2023-03-29 21:49:14 +0300

    sss

--- DZ3.2/Program.cs
+++ DZ3.2/Program.cs
@@ -10,75 +10,117 @@ namespace DZ3._2
     {
         static void Main(string[] args)
         {
-            Pupil pup = new Pupil();
-           
+            
+            Pupil pupil1 = new ExelentPupil();
+            Pupil pupil2 = new GoodPupil();
+            Pupil pupil3 = new GoodPupil();
+            Pupil pupil4 = new BadPupil();
+            ClassRoom Clas = new ClassRoom(pupil1, pupil2, pupil3, pupil4);
+            Clas.Study();
+            Clas.Read();
+            Clas.Write();
+            Clas.Relax();
+            Console.ReadKey();
         }
         
     }
     class ClassRoom
     {
-        Pupil[] pupils;
-        public ClassRoom(Pupil[] pupils)
+         Pupil[] pupils;
+
+        public ClassRoom(params Pupil[] pupils)
         {
             this.pupils = pupils;
+        }
+
+        public void Study()
+        {
+            foreach (Pupil pupil in pupils)
+            {
+                Console.Write("pupil is: ");
+                pupil.Study();
+            }
+        }
+        public void Read()
+        {
+            foreach (Pupil pupil in pupils)
+            {
+                Console.Write("pupil is: ");
+                pupil.Read();
+            }
+        }
+        public void Write()
+        {
+            foreach (Pupil pupil in pupils)
+            {
+                Console.Write("pupil is: ");
+                pupil.Write();
+            }
+        }
+        public void Relax()
+        {
+            foreach (Pupil pupil in pupils)
+            {
+                Console.Write("pupil is: ");
+                pupil.Relax();
+            }
         }
     }
     class Pupil
     {
         public virtual void Study()
         {
-
+            Console.WriteLine("yes");
         }
         public virtual void Read()
         {
-
-
+            Console.WriteLine("yes");
         }
         public virtual void Write()
         {
-
+            Console.WriteLine("yes");
         }
         public virtual void Relax()
         {
-
+            Console.WriteLine("yes");
         }
     }
-    class ExelentPupil:Pupil
+    class ExelentPupil : Pupil
     {
         public override void Study()
         {
-
+            Console.WriteLine("Study hard");
         }
         public override void Read()
         {
-
+            Console.WriteLine("yes");
         }
         public override void Write()
         {
-
+            Console.WriteLine("yes");
         }
         public override void Relax()
         {
-
+            Console.WriteLine("yes");
         }
     }
     class GoodPupil : Pupil
     {
         public override void Study()
         {
-
+            Console.WriteLine("yes");
         }
         public override void Read()
         {
-
+            Console.WriteLine("yes");
         }
         public override void Write()
         {
-
+            Console.WriteLine("yes");
         }
         public override void Relax()
         {
-
+            Console.WriteLine("yes");
         }
 
     }
@@ -86,19 +128,19 @@ namespace DZ3._2
     {
         public override void Study()
         {
-
+            Console.WriteLine("yes");
         }
         public override void Read()
         {
-
+            Console.WriteLine("yes");
         }
         public override void Write()
         {
-
+            Console.WriteLine("yes");
         }
         public override void Relax()
         {
-
+            Console.WriteLine("yes");
         }
 
     }
